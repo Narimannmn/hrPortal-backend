ebfa300234ed3d24f775fe8bd80bb59a4d2f7e8f
EichiMirai <91004927+EichiMirai@users.noreply.github.com>
2024-09-04 17:31:55 +0500

    fix s3

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -17,6 +17,8 @@ import PostCategories from './collections/posts/PostCategories'
 import Posts from './collections/posts/Posts'
 import { webpackBundler } from '@payloadcms/bundler-webpack'
 import { postgresAdapter } from '@payloadcms/db-postgres'
+import { cloudStorage } from '@payloadcms/plugin-cloud-storage'
+import { s3Adapter } from '@payloadcms/plugin-cloud-storage/s3'
 import { slateEditor } from '@payloadcms/richtext-slate'
 import path from 'path'
 import { buildConfig } from 'payload/config'
@@ -53,23 +55,23 @@ export default buildConfig({
 		schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
 	},
 	plugins: [
-		// cloudStorage({
-		// 	collections: {
-		// 		media: {
-		// 			adapter: s3Adapter({
-		// 				config: {
-		// 					region: process.env.S3_REGION,
-		// 					endpoint: process.env.S3_ENDPOINT,
-		// 					credentials: {
-		// 						accessKeyId: process.env.S3_ACCESS_KEY,
-		// 						secretAccessKey: process.env.S3_SECRET_KEY,
-		// 					},
-		// 				},
-		// 				bucket: process.env.S3_BUCKET_NAME,
-		// 			}),
-		// 		},
-		// 	},
-		// }),
+		cloudStorage({
+			collections: {
+				media: {
+					adapter: s3Adapter({
+						config: {
+							region: process.env.S3_REGION,
+							endpoint: process.env.S3_ENDPOINT,
+							credentials: {
+								accessKeyId: process.env.S3_ACCESS_KEY,
+								secretAccessKey: process.env.S3_SECRET_KEY,
+							},
+						},
+						bucket: process.env.S3_BUCKET_NAME,
+					}),
+				},
+			},
+		}),
 	],
 	db: postgresAdapter({
 		pool: {
