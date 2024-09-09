9c25e67edbf1599b96b26ff292172c9a37aca212
Yi <yungpxxp@gmail.com>
2024-09-09 09:23:43 +0000

    Update 2 files
    - /nodemon.json
    - /auto_confirm.sh

---
+++ auto_confirm.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/expect -f
+
+set timeout -1
+
+spawn ts-node src/server.ts -- -I --force
+
+expect {
+    "Is pages_blocks_private_card_banner table created or renamed from another table?" {
+        send -- "\r"   
+        send -- "yes\r" 
+    }
+    timeout {
+    }
+}
+
+expect {
+    "Accept warnings and push schema to database?" {
+        send -- "yes\r" 
+    }
+    timeout {
+    }
+}
+
+interact
