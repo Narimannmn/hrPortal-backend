f69393ec25c3e8d9102a16a0a5f737e10f7ab2ec
Yi <yungpxxp@gmail.com>
2024-09-09 09:54:27 +0000

    Update file auto_confirm.sh

--- auto_confirm.sh
+++ auto_confirm.sh
@@ -4,20 +4,19 @@ set timeout -1
 
 spawn ts-node src/server.ts -- -I --force
 
-expect {
-    "Is pages_blocks_private_card_banner table created or renamed from another table?" {
-        send -- "\r"
-        send -- "yes\r"
-    }
-    timeout {
-    }
-}
-
-expect {
-    "Accept warnings and push schema to database?" {
-        send -- "yes\r"
-    }
-    timeout {
+proc handle_prompt {} {
+    expect {
+        -re "Is .* table created or renamed from another table?" {
+            send -- "\r"   
+            send -- "yes\r" 
+            handle_prompt  
+        }
+        "Accept warnings and push schema to database?" {
+            send -- "yes\r" 
+        }
+        timeout {
+            
+        }
     }
 }
 
