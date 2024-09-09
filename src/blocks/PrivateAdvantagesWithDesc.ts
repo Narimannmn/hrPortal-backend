4801848fcc1476ae28b3b4e46a04c85d5c7d1569
aziz <azizbek.abdimavlonov@gmail.com>
2024-09-09 17:09:28 +0500

    Private components

---
+++ src/blocks/PrivateAdvantagesWithDesc.ts
@@ -0,0 +1,77 @@
+import { Block } from 'payload/types'
+
+const PrivateAdvantagesWithDesc: Block = {
+	slug: 'PrivateAdvantagesWithDesc',
+	labels: {
+		singular: 'Преимущества Diamond club c описанием',
+		plural: 'Преимущества Diamond club с описанием',
+	},
+	fields: [
+		{
+			name: 'cards',
+			label: 'Карточки',
+			type: 'array',
+			fields: [
+				{
+					name: 'img',
+					label: 'Изображение',
+					type: 'upload',
+					relationTo: 'media',
+					required: true,
+				},
+				{
+					name: 'title',
+					label: 'Название',
+					type: 'group',
+					fields: [
+						{
+							name: 'labelRu',
+							label: 'Название (Рус)',
+							type: 'text',
+							required: true,
+						},
+						{
+							name: 'labelEn',
+							label: 'Название (En)',
+							type: 'text',
+							required: true,
+						},
+						{
+							name: 'labelKz',
+							label: 'Название (Қаз)',
+							type: 'text',
+							required: true,
+						},
+					],
+				},
+				{
+					name: 'description',
+					label: 'Описание',
+					type: 'group',
+					fields: [
+						{
+							name: 'labelRu',
+							label: 'Название (Рус)',
+							type: 'text',
+							required: true,
+						},
+						{
+							name: 'labelEn',
+							label: 'Название (En)',
+							type: 'text',
+							required: true,
+						},
+						{
+							name: 'labelKz',
+							label: 'Название (Қаз)',
+							type: 'text',
+							required: true,
+						},
+					],
+				},
+			],
+		}
+	],
+}
+
+export default PrivateAdvantagesWithDesc
