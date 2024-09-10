9213741ca4f4012efbda05a27596f35c466059ed
Yi <yungpxxp@gmail.com>
2024-09-10 14:13:35 +0000

    Update file setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 
-# Install expect
 apk add --no-cache expect
 
-# Make sure auto_confirm.sh is executable
 chmod +x ./auto_confirm.sh
 
-# Set npm proxy configurations
 npm config set proxy 'http://MSansyzb:%21%40%23Eubank%40%21%23%23@proxy01.eub.kz:8080'
 npm config set https-proxy 'http://MSansyzb:%21%40%23Eubank%40%21%23%23@proxy01.eub.kz:8080'
 
-# Clean npm cache and force reinstall packages
 npm cache clean --force
 npm install --force --verbose
 
-# Run the development server
 echo "Running"
 npm run dev
