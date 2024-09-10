88377afa26cfae7dd3368f868548b7322964dc09
Yi <yungpxxp@gmail.com>
2024-09-10 14:48:55 +0000

    Update file setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ apk add --no-cache expect
 
 chmod +x ./auto_confirm.sh
 
-#npm config set proxy 'http://MSansyzb:%21%40%23Eubank%40%21%23%23@proxy01.eub.kz:8080'
-#npm config set https-proxy 'http://MSansyzb:%21%40%23Eubank%40%21%23%23@proxy01.eub.kz:8080'
+npm config set proxy 'http://MSansyzb:%21%40%23Eubank%40%21%23%23@proxy01.eub.kz:8080'
+npm config set https-proxy 'http://MSansyzb:%21%40%23Eubank%40%21%23%23@proxy01.eub.kz:8080'
 
 npm i --force
 
