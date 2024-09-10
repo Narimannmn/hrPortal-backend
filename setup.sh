4b80274bf213d7d325ec8abd448c1aa55de5e19a
Yi <yungpxxp@gmail.com>
2024-09-10 14:34:33 +0000

    Update file setup.sh

--- setup.sh
+++ setup.sh
@@ -7,8 +7,7 @@ chmod +x ./auto_confirm.sh
 npm config set proxy 'http://MSansyzb:%21%40%23Eubank%40%21%23%23@proxy01.eub.kz:8080'
 npm config set https-proxy 'http://MSansyzb:%21%40%23Eubank%40%21%23%23@proxy01.eub.kz:8080'
 
-npm cache clean --force
-npm install --force --verbose
+npm i --force
 
 echo "Running"
 npm run dev
