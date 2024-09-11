f1ce4885a7088b9ab89b9ea345756aa8a1b4b426
Yi <yungpxxp@gmail.com>
2024-09-11 10:59:45 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,13 +7,16 @@ chmod +x ./auto_confirm.sh
 npm config set proxy 'http://MSansyzb:%21%40%23Eubank%40%21%23%23@proxy01.eub.kz:8080'
 npm config set https-proxy 'http://MSansyzb:%21%40%23Eubank%40%21%23%23@proxy01.eub.kz:8080'
 
+yarn config set proxy 'http://MSansyzb:%21%40%23Eubank%40%21%23%23@proxy01.eub.kz:8080'
+yarn config set https-proxy 'http://MSansyzb:%21%40%23Eubank%40%21%23%23@proxy01.eub.kz:8080'
+
 echo "HTTP_PROXY: $HTTP_PROXY"
 echo "HTTPS_PROXY: $HTTPS_PROXY"
 
 npm config list
 
 yarn install --force
-yarn install sharp
+yarn add sharp
 
 echo "Running!"
 yarn run dev
