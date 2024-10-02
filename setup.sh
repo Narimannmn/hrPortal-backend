5cb7e3178fab179e20df7a76d60c64d04f60288d
Magzhan Sansyzbayev <genshiprod@gmail.com>
2024-10-02 08:07:31 +0000

    Update file setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-apk add --no-cache expect vips vips-dev build-base
+apk add --no-cache expect vips vips-dev build-base python3
 
 chmod +x ./auto_confirm.sh
 chmod -R 755 /home/node/app/auto_confirm.sh
 
 npm config set proxy 'http://MSansyzb:%21%40%23Eubank%40%21%23%23@proxy01.eub.kz:8080'
-npm config set https-proxy 'http://MSansyzb:%21%40%23Eubank%40%21%23%23@proxy01.eub.kz:8080'
+npm config set https-proxy 'https://MSansyzb:%21%40%23Eubank%40%21%23%23@proxy01.eub.kz:8080'
 
 yarn config set proxy 'http://MSansyzb:%21%40%23Eubank%40%21%23%23@proxy01.eub.kz:8080'
 yarn config set https-proxy 'http://MSansyzb:%21%40%23Eubank%40%21%23%23@proxy01.eub.kz:8080'
